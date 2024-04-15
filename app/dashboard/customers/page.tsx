e8787dc8ced80d9ca1d4c8355df81aa05b1d5fca
eddrickk <eddricklumine@gmail.com>
2024-04-15 09:00:08 +0700

    finishing

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,35 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import CustomersTable from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+ 
+export default async function Page({
+        searchParams,
+    }: {
+        searchParams?: {
+            query?: string;
+            page?: string;
+        };
+    }) {
+    
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+
+    const customers = await fetchFilteredCustomers(query);
+
+    return (
+        <div className="w-full">
+            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+                <CustomersTable customers={customers} />
+            </Suspense>
+        </div>
+    );
 }
